21f8b27102d2a3a3a796e3b4bf0651a006336328
Alex Eesley <alexandra.eesley@gmail.com>
2020-11-24 12:27:59 -0600

    added contact section

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -4,8 +4,15 @@ import "./style.css";
 function Contact(props) {
   return (
     <div className="card">
-      <h1>Contact Page!!!</h1>
+      <h1>Let's Connect!</h1>
+      <h4>Email</h4>
+      <a href = "mailto: alex@eesleyconsulting.com" target="_blank">Send me an email.</a>
+      <h4>Linkedin</h4>
+      <a href="https://www.linkedin.com/in/alexandra-eesley-0998a81a/" target="_blank">See my past experience.</a>
+      <h4>Github</h4>
+      <a href="https://github.com/aeesley" target="_blank">Explore my projects.</a>
     </div>
+  
   );
 }
 
